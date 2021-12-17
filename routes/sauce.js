d3fe0b61e945f422f43f7902fbdac6342bbd082e
Tamara Wenner <tamara-76@hotmail.fr>
2021-12-18 00:43:18 +0100

    Folders controllers, models and routes added. Test on getAllSauces and createSauce ok

---
+++ routes/sauce.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+const sauceCtrl = require('../controllers/sauce')
+//const auth = require('../middleware/auth');
+
+
+router.get('/', sauceCtrl.getAllSauces);
+router.post('/', sauceCtrl.createSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.put('/:id', sauceCtrl.modifySauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
+
+
+module.exports = router;
