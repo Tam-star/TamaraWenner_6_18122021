2a3c09906185022f25abcca60aef5b0b41071e00
Tamara Wenner <tamara-76@hotmail.fr>
2021-12-19 22:09:37 +0100

    Added a regex to userSchema

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -42,29 +42,29 @@ exports.modifySauce = (req, res, next) => {
             return res.status(404).json({ error: 'Wrong id' })
         }
         //Prevent someone from changing its likes 
-        if(req.body.likes || req.body.dislikes || req.body.usersLiked || req.body.usersDisliked){
+        if (req.body.likes || req.body.dislikes || req.body.usersLiked || req.body.usersDisliked) {
             return res.status(403).json({ error: 'Forbidden request' })
         }
         Sauce.findOne({ _id: req.params.id })
-        .then( sauce => {
-            if (!sauce) {
-                return res.status(404).json({ error: 'Wrong id' })
-            }
-             //On récupère le userId créé dans le middleware auth et on le compare à l'id de sauce
-             if (sauce.userId !== req.auth.userId) {
-                return res.status(403).json({ error: 'Forbidden request' })
-            }
-            const sauceObject = req.file ? {
-                ...JSON.parse(req.body.sauce),
-                imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-            } : { ...req.body }
-            
-            Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-                .then(() => res.status(200).json({ message: 'The sauce has been modified !' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch()
-    } catch (e){ 
+            .then(sauce => {
+                if (!sauce) {
+                    return res.status(404).json({ error: 'Wrong id' })
+                }
+                //Get the userId created in the middleware auth and compare it to the userId of the person who created the sauce
+                if (sauce.userId !== req.auth.userId) {
+                    return res.status(403).json({ error: 'Forbidden request' })
+                }
+                const sauceObject = req.file ? {
+                    ...JSON.parse(req.body.sauce),
+                    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                } : { ...req.body }
+
+                Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'The sauce has been modified !' }))
+                    .catch(error => res.status(400).json({ error }));
+            })
+            .catch(error => res.status(400).json("Bad request"))
+    } catch (e) {
         return res.status(500).json({ message: e.message })
     }
 
@@ -73,16 +73,16 @@ exports.modifySauce = (req, res, next) => {
 
 exports.deleteSauce = (req, res, next) => {
 
-    //Gère le CastError si quelqu'un essaie d'inventer un id qui ne peut pas correspondre à l'ObjectId de MongoDB
+    //Catch CastError if someone tries to invent an id which cannot be a MongoDB ObjectId 
     if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
         return res.status(404).json({ error: 'Wrong id' })
     }
     Sauce.findOne({ _id: req.params.id }).then(
         sauce => {
             if (!sauce) {
-                return res.status(401).json({ error: 'Wrong id' })
+                return res.status(404).json({ error: 'Wrong id' })
             }
-            //On récupère le userId créé dans le middleware auth
+            //Get the userId created in the middleware auth
             if (sauce.userId !== req.auth.userId) {
                 return res.status(403).json({ error: 'Forbidden request' })
             }
@@ -96,122 +96,130 @@ exports.deleteSauce = (req, res, next) => {
         }
     ).catch(error => {
         console.log("Error  : " + error)
-        res.status(500).json("Server error")
+        res.status(400).json("Bad request")
     })
 };
 
 
 exports.updateLikeSauce = (req, res, next) => {
     //Check that request has a userID and a like elements
-    if(!req.body.userId || !req.body.like){
+    if (!req.body.userId || !req.body.like) {
         return res.status(400).json({ error: 'Invalid request' })
     }
-    let userId = req.body.userId
-    
-    //Get the data from the sauce to find out if the user has already liked it or disliked it
-    Sauce.findOne({ _id: req.params.id }).then(sauce => {
-        //If the user likes the sauce
-        if (req.body.like === 1) {
-            //The user already liked the sauce
-            if (sauce.usersLiked.includes(userId)) {
-                res.status(208).json({ message: 'You already liked the sauce !' })
-            }
-            //The user disliked the sauce before
-            else if (sauce.usersDisliked.includes(userId)) {
-                Sauce.updateOne({
-                    _id: req.params.id
-                },
-                    {
-                        $addToSet: { usersLiked: userId },
-                        $pull: { usersDisliked: userId },
-                        $inc: { likes: 1, dislikes: -1 }
-                    }
-                )
-                    .then(() => res.status(200).json({ message: 'Like added ! It seems you changed your mind' }))
-                    .catch(error => res.status(400).json({ error }));
-            }
-            //The user did not vote on like or dislike before
-            else {
-                Sauce.updateOne({
-                    _id: req.params.id
-                },
-                    {
-                        $addToSet: { usersLiked: userId },
-                        $inc: { likes: 1 }
-                    }
-                )
-                    .then(() => res.status(200).json({ message: 'Like added' }))
-                    .catch(error => res.status(400).json({ error }));
-            }
-
-        }
-        //If the user cancel his like or dislike
-        else if (req.body.like === 0) {
-            //The user cancel his like
-            if (sauce.usersLiked.includes(userId)) {
-                Sauce.updateOne({
-                    _id: req.params.id
-                }, {
-                    $pull: { usersLiked: userId, usersDisliked: userId },
-                    $inc: { likes: -1 }
-                })
-                    .then(() => res.status(200).json({ message: 'Like removed' }))
-                    .catch(error => res.status(400).json({ error }));
-            }
-            //The user cancel his dislike
-            else if (sauce.usersDisliked.includes(userId)) {
-                Sauce.updateOne({
-                    _id: req.params.id
-                }, {
-                    $pull: { usersLiked: userId, usersDisliked: userId },
-                    $inc: { dislikes: -1 }
-                })
-                    .then(() => res.status(200).json({ message: 'Dislike removed' }))
-                    .catch(error => res.status(400).json({ error }));
-            }
-            else {
-                res.status(208).json({ message: 'No changes' })
-            }
-
-        }
-        //L'utilisateur n'aime pas la sauce
-        else if (req.body.like === -1) {
-            if (sauce.usersDisliked.includes(userId)) {
-                res.status(208).json({ message: 'You already don\'t like the sauce' })
-            }
-            else if (sauce.usersLiked.includes(userId)) {
-                Sauce.updateOne({
-                    _id: req.params.id
-                },
-                    {
-                        $addToSet: { usersDisliked: userId },
-                        $pull: { usersLiked: userId },
-                        $inc: { likes: -1, dislikes: 1 }
-                    }
-                )
-                    .then(() => res.status(200).json({ message: 'Dislike added ! It seems you changed your mind' }))
-                    .catch(error => res.status(400).json({ error }));
-            }
-            //Lorsque la personne n'avait pas encore donné son avis sur la sauce
-            else {
-                Sauce.updateOne({
-                    _id: req.params.id
-                },
-                    {
-                        $addToSet: { usersDisliked: userId },
-                        $inc: { dislikes: 1 }
-                    }
-                )
-                    .then(() => res.status(200).json({ message: 'Dislike added !' }))
-                    .catch(error => res.status(400).json({ error }));
-            }
-        }
-        else {
-            res.status(400).json('Something is wrong');
-        }
-
+    //Catch CastError
+    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+        return res.status(404).json({ error: 'Wrong id' })
     }
-    )
+    let userId = req.body.userId
+
+    //Get the data from the sauce to find out if the user has already liked it or disliked it
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            if (!sauce) {
+                return res.status(404).json({ error: 'Wrong id' })
+            }
+            //If the user likes the sauce
+            if (req.body.like === 1) {
+                //The user already liked the sauce
+                if (sauce.usersLiked.includes(userId)) {
+                    res.status(208).json({ message: 'You already liked the sauce !' })
+                }
+                //The user disliked the sauce before
+                else if (sauce.usersDisliked.includes(userId)) {
+                    Sauce.updateOne({
+                        _id: req.params.id
+                    },
+                        {
+                            $addToSet: { usersLiked: userId },
+                            $pull: { usersDisliked: userId },
+                            $inc: { likes: 1, dislikes: -1 }
+                        }
+                    )
+                        .then(() => res.status(200).json({ message: 'Like added ! It seems you changed your mind' }))
+                        .catch(error => res.status(400).json({ error }));
+                }
+                //The user did not vote on like or dislike before
+                else {
+                    Sauce.updateOne({
+                        _id: req.params.id
+                    },
+                        {
+                            $addToSet: { usersLiked: userId },
+                            $inc: { likes: 1 }
+                        }
+                    )
+                        .then(() => res.status(200).json({ message: 'Like added' }))
+                        .catch(error => res.status(400).json({ error }));
+                }
+
+            }
+            //If the user cancel his like or dislike
+            else if (req.body.like === 0) {
+                //The user cancel his like
+                if (sauce.usersLiked.includes(userId)) {
+                    Sauce.updateOne({
+                        _id: req.params.id
+                    }, {
+                        $pull: { usersLiked: userId, usersDisliked: userId },
+                        $inc: { likes: -1 }
+                    })
+                        .then(() => res.status(200).json({ message: 'Like removed' }))
+                        .catch(error => res.status(400).json({ error }));
+                }
+                //The user cancel his dislike
+                else if (sauce.usersDisliked.includes(userId)) {
+                    Sauce.updateOne({
+                        _id: req.params.id
+                    }, {
+                        $pull: { usersLiked: userId, usersDisliked: userId },
+                        $inc: { dislikes: -1 }
+                    })
+                        .then(() => res.status(200).json({ message: 'Dislike removed' }))
+                        .catch(error => res.status(400).json({ error }));
+                }
+                else {
+                    res.status(208).json({ message: 'No changes' })
+                }
+
+            }
+            //User doesn't like the sauce
+            else if (req.body.like === -1) {
+                if (sauce.usersDisliked.includes(userId)) {
+                    res.status(208).json({ message: 'You already don\'t like the sauce' })
+                }
+                else if (sauce.usersLiked.includes(userId)) {
+                    Sauce.updateOne({
+                        _id: req.params.id
+                    },
+                        {
+                            $addToSet: { usersDisliked: userId },
+                            $pull: { usersLiked: userId },
+                            $inc: { likes: -1, dislikes: 1 }
+                        }
+                    )
+                        .then(() => res.status(200).json({ message: 'Dislike added ! It seems you changed your mind' }))
+                        .catch(error => res.status(400).json({ error }));
+                }
+                //The user did not like nor dislike the sauce before
+                else {
+                    Sauce.updateOne({
+                        _id: req.params.id
+                    },
+                        {
+                            $addToSet: { usersDisliked: userId },
+                            $inc: { dislikes: 1 }
+                        }
+                    )
+                        .then(() => res.status(200).json({ message: 'Dislike added !' }))
+                        .catch(error => res.status(400).json({ error }));
+                }
+            }
+            else {
+                res.status(400).json('Bad request');
+            }
+
+        }
+        ).catch(error => res.status(400).json('Bad request') )
 
 }
 
--- models/user.js
+++ models/user.js
@@ -2,11 +2,21 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, validate : [validateEmail, 'Email non valide'] },
     password: { type: String, required: true }
 })
 
 //Vérifie l'unicité de l'email
 userSchema.plugin(uniqueValidator);
+
+function validateEmail(email) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.match(mailformat)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 module.exports = mongoose.model('User', userSchema)
